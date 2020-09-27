844a7434ccdfb4bea15458bfdd2fbea9da86af26
Gtes <gates1993@gmail.com>
2020-09-27 14:54:16 +0300

    28. Using a Logo in our Application

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import burgerLogo from "../../assets/Images/28.1 burger-logo.png";
+
+import classes from "./Logo.module.css"
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} />
+    </div>
+);
+
+export default logo;
